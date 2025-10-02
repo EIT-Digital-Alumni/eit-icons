dcc9d178683a232eff6907ef859e254cacc4ddba
Konrad <git@konrad.online>
2025-10-02 09:05:05 +0200

    Fix: Apply scaling when with-circle is false
    Apply scaling and hide circle when with-circle is false. Also fixes
    incorrect background color when with-circle is true and background
    attribute is present.

--- eit-icons.js
+++ eit-icons.js
@@ -1,15 +1,15 @@
 class EitIcons extends HTMLElement {
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: 'open' });
+    this.shadow = this.attachShadow({ mode: "open" });
   }
 
   static get observedAttributes() {
-    return ['name', 'with-circle', 'background'];
+    return ["name", "with-circle", "background"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'name' || name === 'with-circle' || name === 'background') {
+    if (name === "name" || name === "with-circle" || name === "background") {
       this.render();
     }
   }
@@ -29,33 +29,39 @@ class EitIcons extends HTMLElement {
   }
 
   async render() {
-    const iconName = this.getAttribute('name');
-    const withCircle = this.hasAttribute('with-circle');
-    const background = this.getAttribute('background') || 'currentColor';
+    const iconName = this.getAttribute("name");
+    const withCircle = this.hasAttribute("with-circle");
+    const background = this.getAttribute("background");
 
     let svgContent = await this.fetchIcon(iconName);
 
-    if (withCircle) {
+    if (withCircle && background != null) {
       // Apply circle styles and set color to white
       const circleStyle = `
         .eit-icon-circle {
           fill: ${background};
-          stroke: white;
+          stroke: ${background};
         }
         .eit-icon {
           color: white;
         }
       `;
 
-      svgContent = svgContent.replace('<g class="eit-icon">', `<g class="eit-icon" style="color: white;">`);
+      svgContent = svgContent.replace(
+        '<g class="eit-icon">',
+        `<g class="eit-icon" style="color: white;">`,
+      );
 
       // Inject the style into the SVG
-      const styleStart = '<style>';
-      const styleEnd = '</style>';
-      svgContent = svgContent.replace('<svg', `<svg><defs>${styleStart}${circleStyle}${styleEnd}</defs>`);
+      const styleStart = "<style>";
+      const styleEnd = "</style>";
+      svgContent = svgContent.replace(
+        "<svg",
+        `<svg><defs>${styleStart}${circleStyle}${styleEnd}</defs>`,
+      );
+    }
 
-
-    } else {
+    if (!withCircle) {
       // Apply scaling and hide circle
       const scalingStyle = `
         .eit-icon {
@@ -66,10 +72,13 @@ class EitIcons extends HTMLElement {
           display: none;
         }
       `;
-       // Inject the style into the SVG
-       const styleStart = '<style>';
-       const styleEnd = '</style>';
-       svgContent = svgContent.replace('<svg', `<svg><defs>${styleStart}${scalingStyle}${styleEnd}</defs>`);
+      // Inject the style into the SVG
+      const styleStart = "<style>";
+      const styleEnd = "</style>";
+      svgContent = svgContent.replace(
+        "<svg",
+        `<svg><defs>${styleStart}${scalingStyle}${styleEnd}</defs>`,
+      );
     }
 
     this.shadow.innerHTML = `
@@ -82,4 +91,4 @@ class EitIcons extends HTMLElement {
   }
 }
 
-customElements.define('eit-icons', EitIcons);
+customElements.define("eit-icons", EitIcons);
