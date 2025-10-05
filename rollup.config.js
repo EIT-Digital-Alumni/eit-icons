587e27c508eaa00acc218637d29448dfbaf963bc
Konrad <git@konrad.online>
2025-10-05 11:28:47 +0200

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from '@rollup/plugin-node-resolve';
+import resolve from "@rollup/plugin-node-resolve";
 
 export default {
-  input: 'js/index.js',        // entry file
+  input: "src/index.js", // entry file
   output: {
-    file: 'dist/index.bundle.js',
-    format: 'iife',                   // classic browser format
+    file: "dist/index.bundle.js",
+    format: "iife", // classic browser format
   },
   plugins: [resolve()],
 };
